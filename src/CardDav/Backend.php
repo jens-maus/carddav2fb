8cfa28e8b1386db8438baa05a5b4d56712de6c02
andig <cpuidle@gmx.de>
2019-03-24 11:17:04 +0100

    wip

--- src/CardDav/Backend.php
+++ src/CardDav/Backend.php
@@ -4,6 +4,7 @@ namespace Andig\CardDav;
 
 use Andig\Http\ClientTrait;
 use Andig\Vcard\Parser;
+use Sabre\VObject\Reader;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use \stdClass;
@@ -172,6 +173,10 @@ EOD
                 $vcard = $this->parseVcardFromContent($content);
                 $vcard = $this->enrichVcard($vcard);
                 $cards[] = $vcard;
+
+                $c = Reader::read($content);
+                print_r($c->children());
+                die;
 
                 $this->progress();
             }
